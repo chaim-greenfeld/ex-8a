7a000e6eab0d692de90b976c6a81e12d4138b05d
chaim greenfeld <ix64248@gmail.com>
2025-06-22 21:46:00 +0300

    q4

--- q1.py
+++ q1.py
@@ -1,4 +1,13 @@
-for i, x in enumerate([5, 4, 3, 2, 1]):
-    print((i + 1) * x)
+#q2
+#for i, x in enumerate([5, 4, 3, 2, 1]):
+#    print((i + 1) * x)
     #הקוד מכפיל את המיקום ועוד אחד בערך
     #הפלט: 5 8 9 8 5
+
+
+#q4
+for i in range(10):
+
+    for j in range(1, 11):
+        print(j + i*10, end="  ")
+    print()
